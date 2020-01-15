1e3a7fb03b822e24c61ad009f656eae4d3821162
rodriguezoswaldo <owner2303@gmail.com>
2020-01-15 11:20:51 -0500

    add line 9 in scripts to update color typed by the user

--- week2/scripts/script.js
+++ week2/scripts/script.js
@@ -6,7 +6,6 @@ function changeColor(){
     var div1 = document.getElementById('div1');
     var strColor = document.getElementById('newColor').value;
     console.log(strColor);
-    if(strColor!== null){
-        console.log('Hello World!');
-    }
+    div1.style.backgroundColor = strColor;
+   
 }

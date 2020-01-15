36f8f40ffce1d77c2e70fed861986f1729509270
rodriguezoswaldo <owner2303@gmail.com>
2020-01-15 01:27:38 -0500

    coomit

--- week2/scripts/script.js
+++ week2/scripts/script.js
@@ -1,3 +1,12 @@
 function buttonFunction(){
     alert('Clicked');
 }
+
+function changeColor(){
+    var div1 = document.getElementById('div1');
+    var strColor = document.getElementById('newColor').value;
+    console.log(strColor);
+    if(strColor!== null){
+        console.log('Hello World!');
+    }
+}

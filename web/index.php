3e842a3c449c953136727c76555b330167817fee
rodriguezoswaldo <owner2303@gmail.com>
2020-01-16 17:08:47 -0500

    udpdating index page

--- web/index.php
+++ web/index.php
@@ -6,9 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="styles/styles.css">
-
-    <!------ Include the above in your HEAD tag ---------->
-    <!------ Include the above in your HEAD tag ---------->
     <title>Home Page - Oswaldo Rodríguez</title>
 </head>
 <body>
@@ -34,6 +31,12 @@
           </div>
         </div>
       </div>
+
+      <footer>
+          <?php 
+          include "../modules/footer.php";
+            ?>
+          </footer>
 </body>
 </html>
 
---
+++ web/modules/nav.php
@@ -0,0 +1,20 @@
+<ul>
+                <li>
+                    <a href="/index.php" title="Acme Home Page"class="active">Home</a>
+                </li>
+                <li>
+                    <a href="#" title="Cannons in our inventory">Cannon</a>
+                </li>
+                <li>
+                    <a href="#" title="Explosives in our inventory">Explosive</a>
+                </li>
+                <li>
+                    <a href="#" title="miscellaneus items in our inventory">Misc</a>
+                </li>
+                <li>
+                    <a href="#" title="Rockets in our inventory">Rocket</a>
+                </li>
+                <li>
+                    <a href="#" title="Traps in our inventory">Trap</a>
+                </li>
+            </ul>

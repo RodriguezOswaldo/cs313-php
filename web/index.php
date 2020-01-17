afa441df88bf4c741fd0fbfca5bf22cd1be925b5
rodriguezoswaldo <owner2303@gmail.com>
2020-01-17 00:36:13 -0500

    it seems to be working now, updating links

--- web/index.php
+++ web/index.php
@@ -18,13 +18,13 @@
         <div  class="row">
             <div id="div1" class="col-sm">
                 <div class="box">
-                    <h3><a href="about/aboutme.html">About me</a></h3> 
+                    <h3><a href="about/aboutme.php">About me</a></h3> 
                     <p>In this link you will find more information about the developer.</p>
                 </div>
             </div>
             <div id="div2"  class="col-sm">
                 <div class="box">
-                    <h3><a href="assignments/assignments.html">Assignments</a></h3> 
+                    <h3><a href="assignments/assignments.php">Assignments</a></h3> 
                     <p>Click to view previous and new assignments..</p>
                     <p><a  target="_blank" href="https://github.com/RodriguezOswaldo">Github page</a></p>
                 </div>

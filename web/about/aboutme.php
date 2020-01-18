8c63b4e316ddb6ac35b198bf65443bd6d050b274
rodriguezoswaldo <owner2303@gmail.com>
2020-01-18 01:29:25 -0500

    databases update

--- web/about/aboutme.php
+++ web/about/aboutme.php
@@ -82,7 +82,7 @@
             <img src="programming.png" alt="A laptop used by a web developer" title="Photo by Fatos Bytyqi on Unsplash.com">
             <p>I have been programming for the last couple years. I have worked with several programming
                 languages and technologies, included but not limited to HTML, CSS, JS, PHP, jQuery,
-                Visual Studio, C#, Databases and others. </p>
+                Visual Studio, C#, Microsoft SQL Server, Oracle database, phpMyAdmin and others. </p>
             </p>
         </section>
     </div>

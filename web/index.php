90dd137b2a61d22e9e4052beb12d249c534a41be
rodriguezoswaldo <owner2303@gmail.com>
2020-01-18 11:50:24 -0500

    styles

--- web/index.php
+++ web/index.php
@@ -34,6 +34,7 @@
         </div>
     
 </div>
+<hr>
 <footer>
         <p>&copy; <span id="year"></span> | <a title="link to my GitHub page" target="_blank" href="https://github.com/RodriguezOswaldo">Oswaldo Rodríguez S.</a> | South Carolina | <a  target="_blank" title="link to my BYU-I website" href="http://www.byui.edu/online">BYU-I Online Learning.</a></p>
 </footer>

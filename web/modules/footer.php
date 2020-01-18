17a7e3882096dd819aa9da305a156a1b7e6cb69a
rodriguezoswaldo <owner2303@gmail.com>
2020-01-18 13:13:35 -0500

    shared footer

--- web/modules/footer.php
+++ web/modules/footer.php
@@ -1,7 +1,3 @@
-<p>&copy; <span id="year"><?php echo date("Y"); ?></span> | <a title="link to my GitHub page" target="_blank" href="https://github.com/RodriguezOswaldo">Oswaldo
+<p>&copy;Hello World! <span id="year"><?php echo date("Y"); ?></span> | <a title="link to my GitHub page" target="_blank" href="https://github.com/RodriguezOswaldo">Oswaldo
                 Rodríguez S.</a> | South Carolina | <a target="_blank" title="link to my BYU-I website" href="http://www.byui.edu/online">BYU-I
                 Online Learning.</a></p>
-        <?php/* 
-        include $_SERVER['DOCUMENT_ROOT']."/CS313-PHP/web/modules/footer.php";
-        */
-        ?>

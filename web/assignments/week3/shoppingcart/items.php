c016ecc77a2e20b90df4720db3f168750a702a7c
rodriguezoswaldo <owner2303@gmail.com>
2020-01-25 23:21:56 -0500

    adding head.php file and including it to the files

--- web/assignments/week3/shoppingcart/items.php
+++ web/assignments/week3/shoppingcart/items.php
@@ -12,15 +12,11 @@ $_SESSION['name'] = $name;
 <html lang="en">
 
 <head>
+    <?php 
+        include '../shoppingcart/modules/head.php';
+    ?>
 
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-        crossorigin="anonymous">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-        crossorigin="anonymous">    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="styles/styles.css">
+   
     <title>Clone Protocol 66 - Online Store</title>
   
 </head>

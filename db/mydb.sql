775f3680665c742f45c1ecbf924d6caedbb5d268
rodriguezoswaldo <owner2303@gmail.com>
2020-02-01 16:48:57 -0500

    adding foreign keys using the references keyword

--- db/mydb.sql
+++ db/mydb.sql
@@ -1,18 +1,18 @@
 CREATE TABLE book(
-    book_id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     book_title VARCHAR(100) NOT NULL UNIQUE,
     author VARCHAR(100) NOT NULL UNIQUE,
     book_release_year TIMESTAMP,
     book_category_name VARCHAR(25) NOT NULL,
-    client_id INT NOT NULL,
+    client_id INT NOT NULL
 );
 CREATE TABLE book_category(
-    book_category_id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     book_category_name VARCHAR(25),
-    book_title VARCHAR(25), 
+    book_id int UNIQUE REFERENCES book(id)
 );
 CREATE TABLE client(
-    client_id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     client_name VARCHAR(100) NOT NULL UNIQUE,
-    book_title VARCHAR(100) NOT NULL,
+    book_title VARCHAR(100) NOT NULL REFERENCES book(book_title)
 );

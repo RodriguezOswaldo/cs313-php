ea7cc4dd1cbd08b701bcd9a56731b288cc11b32d
rodriguezoswaldo <owner2303@gmail.com>
2020-02-12 00:18:59 -0500

    message

--- web/assignments/week6/scripture_topic.php
+++ web/assignments/week6/scripture_topic.php
@@ -1,4 +1,9 @@
-<!DOCTYPE html>
+<?php
+//bringing the connection!
+require "connections.php";
+$db=get_db();
+
+?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -10,13 +15,29 @@
     <h1>Topic Scripture</h1>
     <form action="">
     <label for="book">Book:</label>
-        <input type="text" name="book">
+        <input type="text" id="book" name="book"><br>
     <label for="chapter">Chapter:</label>
-        <input type="text" name="chapter">
-        <input type="text" name="book">
+        <input type="text" id="chapter" name="chapter"><br>
     <label for="verse">Verse:</label>
-        <input type="text" name="verse">
+        <input type="text" id="verse" name="verse"><br>
+        <label for="content" >Content:</label>
         <textarea name="content" id="content" cols="30" rows="10"></textarea>
+        <label for="topics">Topics</label><br>
+        <?php
+            $stmt = $db->prepare('SELECT id, name FROM topic'); 
+            $stmt->execute();
+
+            foreach ($topics as $stmt) 
+            {
+                $id = $topics['id'];
+                $name= $topics['name'];
+
+                echo "<input type='checkbox' name='topics[]' id='topics$id' value='$id'>";
+                echo "<label for='topics$id'>$name</label>";
+            }
+        ?>
+
+        <button >Submit</button>
     </form>
 </body>
 </html>

382606f870be36187d33c1b1ed9537f5bae76c0d
rodriguezoswaldo <owner2303@gmail.com>
2020-02-12 00:34:07 -0500

    sdsd

--- web/assignments/week6/scripture_topic.php
+++ web/assignments/week6/scripture_topic.php
@@ -9,24 +9,25 @@ $db=get_db();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+        crossorigin="anonymous">
     <title>Scriptures</title>
 </head>
 <body>
     <h1>Topic Scripture</h1>
     <form action="" method="post">
-    <label for="book">Book:</label>
+        <label for="book">Book:</label>
         <input type="text" id="book" name="book"><br>
-    <label for="chapter">Chapter:</label>
+        <label for="chapter">Chapter:</label>
         <input type="text" id="chapter" name="chapter"><br>
-    <label for="verse">Verse:</label>
+        <label for="verse">Verse:</label>
         <input type="text" id="verse" name="verse"><br>
-        <label for="content" >Content:</label>
+        <label for="content" >Content:</label><br>
         <textarea name="content" id="content" cols="30" rows="10"></textarea>
         <label for="topics">Topics: </label><br>
         <?php
-            $stmt = $db->prepare('SELECT id, name FROM topic'); 
+            $stmt = $db->prepare('SELECT * FROM topic'); 
             $stmt->execute();
-
             while ($row = $statement->fetch(PDO::FETCH_ASSOC))
             {
                 $id = $row['id'];
@@ -36,7 +37,6 @@ $db=get_db();
                 echo "<label for='topics$id'>$name</label><br>";
             }
         ?>
-
         <button >Submit</button>
     </form>
 </body>

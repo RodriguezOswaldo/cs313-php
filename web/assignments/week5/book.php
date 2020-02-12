033e19aa1e7d6014177d940fbd3577761cb15a01
rodriguezoswaldo <owner2303@gmail.com>
2020-02-11 22:43:43 -0500

    deleting unnecesary tags

--- web/assignments/week5/book.php
+++ web/assignments/week5/book.php
@@ -37,7 +37,7 @@ $db=get_db();
         $category = $row['book_category_name'];
         $client = $row['client_name'];
         // echo "<a href="">$client</a>;
-        echo "<li><p><a href='booksinfo.php?book_id=$id'><strong>Book Title</strong>: $title.  <strong> </strong>\"</a></p></li>";
+        echo "<li><p><a href='booksinfo.php?book_id=$id'> $title</a></p></li>";
     }    
     ?>
     </ul>

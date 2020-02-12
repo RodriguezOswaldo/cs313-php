29f2fa42706744e77194897b8bd5010dd08a8303
rodriguezoswaldo <owner2303@gmail.com>
2020-02-11 22:33:29 -0500

    trying to echo variables

--- web/assignments/week5/booksinfo.php
+++ web/assignments/week5/booksinfo.php
@@ -13,13 +13,13 @@ $stmt = $db->prepare('SELECT b.book_title, b.author, b.book_release_year, b.book
 JOIN client cl ON cl.book_title = b.book_title WHERE b.id =:id;');
 $stmt->bindValue(':id', $book_id, PDO::PARAM_INT);
 $stmt->execute();
-$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$book_rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 $title = $rows[0]['book_title'];
-$author = $rows[1]['author'];
-$year = $rows[2]['book_release_year'];
-$category = $rows[3]['book_category_name'];
-$client  = $rows[4]['client_name'];
+// $author = $rows[1]['author'];
+// $year = $rows[2]['book_release_year'];
+// $category = $rows[3]['book_category_name'];
+// $client  = $rows[4]['client_name'];
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -32,9 +32,13 @@ $client  = $rows[4]['client_name'];
     <h2><?php echo $title; ?></h2>
     <p>
     <?php 
+    foreach($book_rows as $book_row)
+    {
+        $authorName = $book_row['author'];
+        echo '<p>Written by:</strong> $authorName.  </p>';
+    }
     
             echo '<p>$title </p>';
-            echo '<p>Written by:</strong> $author.  </p>';
             echo '<p><strong>Originally Published:</strong> $year. </p>';
             echo '<p><strong>Book Category:</strong>$category.</p>';
             echo '<p><strong>Uploaded by: </strong> $client.</p>';

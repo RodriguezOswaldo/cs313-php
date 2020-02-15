2dcda86c85ecc049618847fecb2527f99dc736ae
rodriguezoswaldo <owner2303@gmail.com>
2020-02-15 17:55:05 -0500

    Ss

--- web/assignments/week5/newbook.php
+++ web/assignments/week5/newbook.php
@@ -4,9 +4,11 @@ if(!isset($_GET['book_id']))
 {
     die('Error, book id not specified');
 }
+echo 'I work';
 $book_id = htmlspecialchars($_GET['book_id']);
 //Get Connections
 require 'connections.php';
+echo 'I work 2 ';
 $db = get_db();
 //From the preparation
 $stmt = $db->prepare('SELECT b.book_title, b.author, b.book_release_year, b.book_category_name, cl.client_name FROM book b 
@@ -14,6 +16,7 @@ JOIN client cl ON cl.book_title = b.book_title WHERE b.id =:id;');
 $stmt->bindValue(':id', $book_id, PDO::PARAM_INT);
 $stmt->execute();
 $book_rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+echo 'I work 3 ';
 ?><!DOCTYPE html>
 <html lang="en">
 <head>

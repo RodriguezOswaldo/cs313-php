84fd728ff08ad583af7f62e198ea25ac0a9bd736
rodriguezoswaldo <owner2303@gmail.com>
2020-02-15 20:05:07 -0500

    ssds

--- web/assignments/week5/addbook.php
+++ web/assignments/week5/addbook.php
@@ -4,15 +4,19 @@ require 'connections.php';
 $db = get_db();
 //From the preparation
 $book_title = htmlspecialchars($_POST['book_title']);
+$book_title = htmlspecialchars($_POST['author']);
+$book_release_year = htmlspecialchars($_POST['book_release_year']);
+$book_category_name = htmlspecialchars($_POST['book_category_name']);
+$client_name = htmlspecialchars($_POST['client_name']);
 
-$stmt = $db->prepare('INSERT INTO book (book_title, author, book_release_year, book_category_name, client_name)
-VALUES (:book_title, :author, :book_release_year, :book_category_name, :client_name);');
-$stmt->bindValue(':book_title', $book_title, PDO::PARAM_STR);
-$stmt->bindValue(':author', $author, PDO::PARAM_STR);
-$stmt->bindValue(':book_release_year', $book_release_year, PDO::PARAM_INT);
-$stmt->bindValue(':book_category_name', $book_category_name, PDO::PARAM_STR);
-$stmt->bindValue(':client_name', $client_name, PDO::PARAM_STR);
-$stmt->execute();
+// $stmt = $db->prepare('INSERT INTO book (book_title, author, book_release_year, book_category_name, client_name)
+// VALUES (:book_title, :author, :book_release_year, :book_category_name, :client_name);');
+// $stmt->bindValue(':book_title', $book_title, PDO::PARAM_STR);
+// $stmt->bindValue(':author', $author, PDO::PARAM_STR);
+// $stmt->bindValue(':book_release_year', $book_release_year, PDO::PARAM_INT);
+// $stmt->bindValue(':book_category_name', $book_category_name, PDO::PARAM_STR);
+// $stmt->bindValue(':client_name', $client_name, PDO::PARAM_STR);
+// $stmt->execute();
 echo 'yo are here\n';
 echo $book_title;
 echo $author;

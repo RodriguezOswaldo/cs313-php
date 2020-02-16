79a7c3e522b6bfd53a213f82bfd0493bf061d15d
rodriguezoswaldo <owner2303@gmail.com>
2020-02-15 21:14:29 -0500

    bootstrap

--- web/assignments/week5/booksinfo.php
+++ web/assignments/week5/booksinfo.php
@@ -19,10 +19,15 @@ $title = $book_rows[0]['book_title'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+        crossorigin="anonymous">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+        crossorigin="anonymous"> 
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Books Info</title>
 </head>
 <body>
+    <div class="container">
     <h2><?php echo $title; ?></h2>
     <p>
     <?php 
@@ -40,5 +45,6 @@ $title = $book_rows[0]['book_title'];
 
     ?>
     </p>
+    </div>
 </body>
 </html>

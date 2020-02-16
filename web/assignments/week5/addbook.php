760b060e209662e6ca8b370e442508f54bbfd9a8
rodriguezoswaldo <owner2303@gmail.com>
2020-02-15 20:27:58 -0500

    troubleshooting 2

--- web/assignments/week5/addbook.php
+++ web/assignments/week5/addbook.php
@@ -10,14 +10,14 @@ require 'connections.php';
 $db = get_db();
 
 
-// $stmt = $db->prepare('INSERT INTO book (book_title, author, book_release_year, book_category_name, client_name)
-// VALUES (:book_title, :author, :book_release_year, :book_category_name, :client_name);');
-// $stmt->bindValue(':book_title', $book_title, PDO::PARAM_STR);
-// $stmt->bindValue(':author', $author, PDO::PARAM_STR);
-// $stmt->bindValue(':book_release_year', $book_release_year, PDO::PARAM_INT);
-// $stmt->bindValue(':book_category_name', $book_category_name, PDO::PARAM_STR);
-// $stmt->bindValue(':client_name', $client_name, PDO::PARAM_STR);
-// $stmt->execute();
+$stmt = $db->prepare('INSERT INTO book (book_title, author, book_release_year, book_category_name, client_name)
+VALUES (:book_title, :author, :book_release_year, :book_category_name, :client_name);');
+$stmt->bindValue(':book_title', $book_title, PDO::PARAM_STR);
+$stmt->bindValue(':author', $author, PDO::PARAM_STR);
+$stmt->bindValue(':book_release_year', $book_release_year, PDO::PARAM_INT);
+$stmt->bindValue(':book_category_name', $book_category_name, PDO::PARAM_STR);
+$stmt->bindValue(':client_name', $client_name, PDO::PARAM_STR);
+$stmt->execute();
 
 echo 'yo are here\n';
 echo $book_title;

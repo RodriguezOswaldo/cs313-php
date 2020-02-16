95672b45151d43aaf6e7614b6373d1925934c5ab
rodriguezoswaldo <owner2303@gmail.com>
2020-02-16 01:59:42 -0500

    push

--- web/assignments/week5/delete.php
+++ web/assignments/week5/delete.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+if(!isset($_GET['book_id']))
+{
+    die('Error, book id not specified');
+}
+$book_id = htmlspecialchars($_GET['book_id']);
 
 //Get Connections
 require 'connections.php';
@@ -7,7 +12,7 @@ require 'connections.php';
 $db = get_db();
 
 $stmt = $db->prepare('DELETE FROM book WHERE id =:book_id');
-$stmt->bindValue(':id', $book_id, PDO::PARAM_INT);
+$stmt->bindValue(':book_id', $book_id, PDO::PARAM_INT);
 $stme->execute();
 
 $new_page = "book.php";

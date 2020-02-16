4d27287560d61e17aad17255f7057abe359feb0f
rodriguezoswaldo <owner2303@gmail.com>
2020-02-16 02:02:55 -0500

    push

--- web/assignments/week5/book.php
+++ web/assignments/week5/book.php
@@ -41,7 +41,7 @@ $db=get_db();
         $category = $row['book_category_name'];
         $client = $row['client_name'];
         // echo "<a href="">$client</a>;
-        echo "<li><a href='booksinfo.php?book_id=$id'>$title</a> <a href='update.php?book_id=$id' class='btn-outline-success'>Update</a> | <button href='delete.php' class='btn-outline-danger'>Delete</button></li>";
+        echo "<li><a href='booksinfo.php?book_id=$id'>$title</a> <a href='update.php?book_id=$id' class='btn-outline-success'>Update</a> | <a href='delete.php' class='btn-outline-danger'>Delete</a></li>";
     }    
     ?>
     </ul>

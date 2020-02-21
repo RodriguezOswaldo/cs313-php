8eb2ce4b6dc7d80baf39760150ddca182b4a79da
rodriguezoswaldo <owner2303@gmail.com>
2020-02-21 17:16:36 -0500

    login credentials

--- web/assignments/week5/newAccount.php
+++ web/assignments/week5/newAccount.php
@@ -5,4 +5,10 @@ $password = $_POST['acct_password'];
  echo " you made it!";
  echo $username;
  echo $password;
+// if username or password are empty display back the register form
+ if(!isset($username) || $username == "" || !isset($password) || $password == "")
+ {
+    header("Location: register.php");
+    die();
+ }
 ?>

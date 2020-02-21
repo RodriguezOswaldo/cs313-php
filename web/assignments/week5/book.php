4fbc587a6f64a62d6ff7162072a919e4403f3eae
rodriguezoswaldo <owner2303@gmail.com>
2020-02-21 16:44:34 -0500

    add new book style

--- web/assignments/week5/book.php
+++ web/assignments/week5/book.php
@@ -19,6 +19,7 @@ $db=get_db();
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
         crossorigin="anonymous"> 
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/assignments/styles/styles.css">
     <title>Books List!</title>
 </head>
 <body>

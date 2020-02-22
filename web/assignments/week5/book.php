11a1bf216a9c49b168dd41c2efd946e46edf0314
rodriguezoswaldo <owner2303@gmail.com>
2020-02-21 19:09:55 -0500

    styles

--- web/assignments/week5/book.php
+++ web/assignments/week5/book.php
@@ -23,7 +23,7 @@ $db=get_db();
     <title>Books List!</title>
 </head>
 <body>
-    <div class="container books_summary">
+    <div class="container books-summary">
     <h2>Full List of Books!</h2>
     <ul>
     <?php

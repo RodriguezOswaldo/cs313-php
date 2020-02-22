6b00bc8f55c1f55495d580853aa79b504a62bfea
rodriguezoswaldo <owner2303@gmail.com>
2020-02-22 08:09:47 -0500

    adding style

--- web/assignments/week5/booksinfo.php
+++ web/assignments/week5/booksinfo.php
@@ -24,6 +24,7 @@ $title = $book_rows[0]['book_title'];
         crossorigin="anonymous">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
         crossorigin="anonymous"> 
+        <link rel="stylesheet" href="/assignments/styles/styles.css">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Books Info</title>
 </head>
